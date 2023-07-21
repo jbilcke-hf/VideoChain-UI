40e9c342f1071cf15fcd70129fae00af8d143f72
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-21 18:09:37 +0200

    fix page scroll

--- src/app/studio/[ownerId]/page.tsx
+++ src/app/studio/[ownerId]/page.tsx
@@ -8,7 +8,7 @@ export default async function StudioPage({ params: { ownerId } }: { params: { ow
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=0.86, maximum-scale=5.0, minimum-scale=0.86" />
       </Head>
-      <main className="dark h-screen w-full flex flex-col items-center bg-stone-900 text-stone-100">
+      <main className="dark fixed inset-0 flex flex-col items-center bg-stone-900 text-stone-10 overflow-y-scroll">
         <Main ownerId={`${ownerId || ""}`} />
       </main>
     </div>

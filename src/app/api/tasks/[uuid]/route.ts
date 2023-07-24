e3cd9574af8589e4d65726ba49d43012cf3a1b7d
Julian Bilcke <julian.bilcke@huggingface.co>
2023-07-24 17:47:46 +0200

    remove useless api

--- src/app/api/tasks/[uuid]/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-import { getVideo } from "@/server"
-
-// TODO: implement some kind of quota system
-export async function GET(req: NextRequest) {
-  return NextResponse.json({
-    video: await getVideo(`${req.url?.split('/').pop() || ""}`)
-  })
-}
